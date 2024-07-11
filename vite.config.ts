a8acff8a6c9696663965ccc6200eb443e32f71a9
juanlunaa <jp3165239@gmail.com>
2024-07-11 12:11:14 -0500

    Change server config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
+	/*server: {
 		host: true, // esto hace que el servidor escuche en todas las interfaces de red
 		port: 3000, // puedes cambiar el puerto si lo deseas
-	},
+	},*/
 });
